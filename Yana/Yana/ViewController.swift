61b5e0184cd992c4d0ddc961b5bac8620260ba53
AndersonAlencar <anderson.sjav@gmail.com>
2020-04-23 10:02:17 -0300

    Start project and add swift lint

---
+++ Yana/Yana/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Yana
+//
+//  Created by Anderson Alencar on 23/04/20.
+//  Copyright © 2020 Anderson Alencar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
